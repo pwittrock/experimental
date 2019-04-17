01cd88dbf2f6db71fd6041bf481d9e488b2e9535
Phillip Wittrock <pwittroc@google.com>
2019-04-17 09:19:29 -0700

    7

--- cmd/triggers/triggers.go
+++ cmd/triggers/triggers.go
@@ -86,53 +86,59 @@ func (s *GitHubEventMonitor) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	switch event := event.(type) {
 	case *github.PushEvent:
-		err = s.DoPush(event)
+		err = s.DoPushEvent(event)
 	}
-
-	fmt.Printf("=====\n\n%+v\n=====\n\n", event)
-
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "error: %v\n", err)
 	}
 
+	fmt.Printf("=====Message=====\n%+v\n=====\n\n", event)
+
 }
 
-func (s *GitHubEventMonitor) DoPush(event *github.PushEvent) error {
-	var doRef string
-
-	if len(*refs) > 0 {
-		found := false
-		for _, ref := range *refs {
-			if ref == event.GetRef() {
-				found = true
-				doRef = event.GetRef()
-				break
-			}
-		}
-		for _, ref := range *refs {
-			if ref == event.GetBaseRef() {
-				doRef = event.GetBaseRef()
-				found = true
-				break
-			}
-		}
-
-		if !found {
-			return nil
-		}
+func (s *GitHubEventMonitor) DoPushEvent(event *github.PushEvent) error {
+	path, err := s.DoPushClone(event)
+	if err != nil {
+		return err
 	}
 
-	fmt.Printf("name: %s\n", event.Repo.GetFullName())
+	objs, err := s.GetResources(event, filepath.Join(path, "apply"), "push")
+	if err != nil {
+		return err
+	}
+	err = s.DoKubectlAll("apply", objs)
+	if err != nil {
+		return err
+	}
+
+	objs, err = s.GetResources(event, filepath.Join(path, "create"), "push")
+	if err != nil {
+		return err
+	}
+	err = s.DoKubectlAll("create", objs)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("done\n")
+	return nil
+}
+
+func (s *GitHubEventMonitor) DoPushClone(event *github.PushEvent) (string, error) {
+	ref := event.GetRef()
+	if len(event.GetBaseRef()) > 0 {
+		ref = event.GetBaseRef()
+	}
 
 	dir, err := ioutil.TempDir(os.TempDir(), "git-clone")
 	if err != nil {
-		return err
+		return "", err
 	}
 	defer os.RemoveAll(dir) // clean up
 
 	err = os.Chdir(dir)
 	if err != nil {
-		return err
+		return "", err
 	}
 
 	r := fmt.Sprintf("https://github.com/%s.git", event.GetRepo().GetFullName())
@@ -143,14 +149,14 @@ func (s *GitHubEventMonitor) DoPush(event *github.PushEvent) error {
 		URL:           r,
 		Progress:      os.Stdout,
 		Depth:         1,
-		ReferenceName: plumbing.ReferenceName(doRef),
+		ReferenceName: plumbing.ReferenceName(ref),
 		Auth: &httpv4.BasicAuth{
 			Username: "", // anything except an empty string
 			Password: string(s.Secret),
 		},
 	})
 	if err != nil {
-		return err
+		return "", err
 	}
 
 	fmt.Printf("cloned %s\n", r)
@@ -158,101 +164,101 @@ func (s *GitHubEventMonitor) DoPush(event *github.PushEvent) error {
 	fmt.Printf("reading files...\n")
 	files, err := ioutil.ReadDir(loc)
 	if err != nil {
-		return err
+		return "", err
 	}
 	for i := range files {
 		file := files[i]
 		fmt.Printf("cloned file: %s\n", file.Name())
 	}
+	return filepath.Join(loc, *path), nil
+}
 
-	tekPath := filepath.Join(loc, *path)
-
-	if _, err := os.Stat(tekPath); os.IsNotExist(err) {
-		fmt.Printf("missing [%s] directory\n", *path)
-		return nil
+func (s *GitHubEventMonitor) GetResources(event *github.PushEvent, path, trigger string) ([]*unstructured.Unstructured, error) {
+	ref := event.GetRef()
+	if len(event.GetBaseRef()) > 0 {
+		ref = event.GetBaseRef()
 	}
 
-	cfgPath := filepath.Join(tekPath, "apply")
-	if _, err := os.Stat(cfgPath); err == nil {
-		fmt.Printf("applying config...\n")
-		cmd := exec.Command("kubectl", "apply", "--filename", cfgPath, "--recursive")
-		cmd.Stderr = os.Stderr
-		cmd.Stdout = os.Stdout
-		err = cmd.Run()
+	t, err := template.ParseGlob(filepath.Join(path, "*.yaml"))
+	if err != nil {
+		return nil, err
+	}
+	buf := &bytes.Buffer{}
+	for _, tmpl := range t.Templates() {
+		err = tmpl.Execute(buf, Data{
+			Ref: strings.Replace(ref, "refs/", "", -1),
+			URL: fmt.Sprintf("https://github.com/%s", event.Repo.GetFullName()),
+		})
 		if err != nil {
-			return err
+			return nil, err
 		}
+		buf.WriteString("\n---\n")
 	}
 
-	runsPath := filepath.Join(tekPath, "create")
-	if _, err := os.Stat(runsPath); err == nil {
-		t, err := template.ParseGlob(filepath.Join(runsPath, "*.yaml"))
-		if err != nil {
-			return err
+	fmt.Printf("found runs\n%s\n", buf.String())
+	objs := strings.Split(string(buf.String()), "---")
+	var configs, match []*unstructured.Unstructured
+	for i := range objs {
+		o := objs[i]
+		if len(strings.TrimSpace(o)) == 0 {
+			continue
 		}
-		buf := &bytes.Buffer{}
-		for _, tmpl := range t.Templates() {
-			err = tmpl.Execute(buf, Data{
-				Ref: strings.Replace(doRef, "refs/", "", -1),
-				URL: r,
-			})
-			if err != nil {
-				return err
-			}
-			buf.WriteString("\n---\n")
+		body := map[string]interface{}{}
+		if err := yaml.Unmarshal([]byte(o), &body); err != nil {
+			return nil, err
 		}
+		configs = append(configs, &unstructured.Unstructured{Object: body})
+	}
 
-		fmt.Printf("found runs\n%s\n", buf.String())
-		objs := strings.Split(string(buf.String()), "---")
-		var configs []*unstructured.Unstructured
-		for i := range objs {
-			o := objs[i]
-			if len(strings.TrimSpace(o)) == 0 {
-				continue
-			}
-			body := map[string]interface{}{}
-			if err := yaml.Unmarshal([]byte(o), &body); err != nil {
-				return err
-			}
-			configs = append(configs, &unstructured.Unstructured{Object: body})
-		}
-
-		var run []*unstructured.Unstructured
-		for i := range configs {
-			config := configs[i]
-			fmt.Printf("found object %s %s\n", config.GetName(), config.GetGenerateName())
-			if v, found := config.GetAnnotations()["tekctl.tektoncd.dev/trigger"]; found {
-				if v == "push" {
-					fmt.Printf("running %s\n", config.GetGenerateName())
-					run = append(run, config)
+	for i := range configs {
+		config := configs[i]
+		fmt.Printf("found object %s %s\n", config.GetName(), config.GetGenerateName())
+		if v, found := config.GetAnnotations()["tekctl.tektoncd.dev/trigger"]; found {
+			for _, p := range strings.Split(v, ",") {
+				if p == trigger {
+					match = append(match, config)
+					break
 				}
 			}
 		}
-
-		for i := range run {
-			// create the run tasks
-			c := exec.Command("kubectl", "create", "-f", "-")
-			m, err := yaml.Marshal(run[i])
-			if err != nil {
-				return err
-			}
-			buf := &bytes.Buffer{}
-			_, err = buf.Write(m)
-			if err != nil {
-				return err
-			}
-			c.Stdin = buf
-			c.Stdout = os.Stdout
-			c.Stderr = os.Stderr
-			err = c.Run()
-			if err != nil {
-				return err
-			}
-		}
 	}
 
-	fmt.Printf("done\n")
+	return match, nil
+}
+
+func (s *GitHubEventMonitor) DoKubectlAll(c string, objs []*unstructured.Unstructured) error {
+	for i := range objs {
+		err := s.DoKubectl(c, objs[i])
+		if err != nil {
+			return err
+		}
+	}
 	return nil
+}
+
+func (s *GitHubEventMonitor) GetBuf(obj *unstructured.Unstructured) (*bytes.Buffer, error) {
+	m, err := yaml.Marshal(obj)
+	if err != nil {
+		return nil, err
+	}
+	buf := &bytes.Buffer{}
+	_, err = buf.Write(m)
+	if err != nil {
+		return nil, err
+	}
+	return buf, err
+}
+
+func (s *GitHubEventMonitor) DoKubectl(c string, obj *unstructured.Unstructured) error {
+	b, err := s.GetBuf(obj)
+	if err != nil {
+		return err
+	}
+	cmd := exec.Command("kubectl", c, "--filename", "-")
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+	cmd.Stdin = b
+	return cmd.Run()
 }
 
 type Data struct {
